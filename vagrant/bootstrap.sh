5fe75f77c5c0c4d8b3060b165322edd6e1f34833
Gergely Varga <spherule@gmail.com>
2016-09-27 17:18:18 -0500

    vagrant update

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,8 +3,9 @@
 apt-get update
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
+apt-add-repository ppa:fkrull/deadsnakes
 apt-get update
-apt-get install -y ansible git
+apt-get install -y ansible git python2.7
 
 git clone https://github.com/sperka/lusheeta.git
 chown -R vagrant:vagrant lusheeta
